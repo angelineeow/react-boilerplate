d9be636701d04e20d19742932aa2cf0f0a139e02
ae1204 <angelineeooww@gmail.com>
2022-12-23 20:11:30 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // module system: 
-import HelloMessage from "./components/HelloMassage"
+import HelloMessage from "./components/HelloMessage"
+import ReactDOM from "react-dom/client";
+import React from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HelloMessage name="Angeline" />)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,55 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-
+//source-map
+//html plugin
+//babel loader
+//css loader
+//dev server
 module.exports = {
-  entry: './lib/index.js',
+  module: {
+    rules: [
+        {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    '@babel/preset-env',
+                    '@babel/preset-react'
+                ]
+            }
+        }
+        },
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"], // order matters here
+        },
+    ]
+  },
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, 'bundled'),
+    filename: 'index.js',
+  },
+  mode:"production", //production mode, size is smaller
+  devtool: "source-map",
+  plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'src/index.html'
+      })
+  ],
+  devServer: {
+    client: {
+        overlay: {
+            warnings: false,
+        },
+    },
+    // static: {
+    //   directory: path.join(__dirname, 'bundled'), // where to bind the bundled file
+    // },
+    compress: true, // compress the bundled file before sending it to the browser
+    port: 9000,
   },
 };
